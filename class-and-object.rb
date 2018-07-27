445b58bf3e72e41771d24a60ac5f799fbadfc424
ashutosh <amxdp18@gmail.com>
2018-07-27 23:31:10 +0530

    global variable added

--- class-and-object.rb
+++ class-and-object.rb
@@ -6,3 +6,22 @@ end
 
 object = Sample.new
 object.hello
+
+#Global variable
+$gv = 10
+class First
+ def print_global
+  puts 'Global variable in First is #$gv' 
+end
+end
+
+class Second
+ def print_global
+ puts 'Here is second time use #$gv' 
+end
+end
+
+a = First.new
+b = Second.new
+a.print_global
+b.print_global
