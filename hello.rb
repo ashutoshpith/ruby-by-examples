e7e614b6267e0661bb3c51aed33d869dfc15d85e
ashutosh <amxdp18@gmail.com>
2018-08-27 16:35:36 +0530

    update hello

--- hello.rb
+++ hello.rb
@@ -1,1 +1,40 @@
 puts "hello earth.I'm here new";
+puts "     /|"
+puts "    / |"
+puts "   /  |"
+puts "  /---|"
+puts " /    |"
+puts "/_____|"
+
+char_name = "  hello maggie  "
+puts ("the character name "+ char_name)
+puts char_name.upcase()
+puts char_name.downcase()
+puts char_name.length()
+puts char_name.strip()
+puts char_name.length()
+puts char_name.include? "name"
+puts char_name.include? "hello"
+puts char_name.include? "hello ma"
+puts char_name[2]
+puts char_name[0, 5]
+puts char_name.index("a")
+puts "hell ji".upcase()
+
+#maths
+puts 3**2
+puts 3^2
+puts 3*2
+num = 20.59
+puts ("my fav num " + num.to_s)
+puts num.abs()
+puts num.round()
+puts num.ceil()
+puts num.floor()
+
+#inputs
+
+a = gets
+puts a
+b = gets.chop()
+puts b
