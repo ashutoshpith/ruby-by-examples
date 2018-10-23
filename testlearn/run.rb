e83bbfc83cc6d33681703b81dcfc8befe78489b9
ashutosh <amxdp18@gmail.com>
2018-10-23 23:24:42 +0530

    loop in ruby

--- testlearn/run.rb
+++ testlearn/run.rb
@@ -27,3 +27,56 @@ when 13 .. 18
   puts 'youth'
 
 end
+
+#loop in ruby
+
+while $age< 10 do
+  puts "inside the loop i = #$age"
+  $age +=1
+end
+puts "break"
+
+begin
+  puts "inside the loop i = #$age"
+  $age +=1
+end while $age<10
+
+puts "break"
+
+until $age > 10 do
+  puts "Inside the loop i =#$age"
+  $age +=1
+end
+
+puts "for loop"
+
+for i in 0 .. 5
+  puts "value of i in for loop is #{i}"
+end
+
+(0 .. 5).each do |i|
+  puts "value of i is #{i}"
+end
+
+for i in 0 .. 5
+  if i>2
+    break
+  end
+  puts "value of i is #{i}"
+end
+puts "comments"
+
+for i in 0 .. 5
+  if i <2
+    next
+  end
+  puts "Value of i is #{i}"
+end
+
+puts "comments"
+
+
+for i in 0..5
+  retry if i > 2
+  puts "Value of local variable is #{i}"
+end
